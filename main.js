04296f4b14cefedcea6e3816662ee031dbe41686
Dev_Trumic <dev@dev.dev>
2024-08-06 09:44:01 +0200

    finished

---
+++ main.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const svg = document.querySelector("svg");
+const path = svg.querySelector("path");
+const links = document.querySelectorAll(".linkwrappers a");
+
+renderMenu();
+
+function renderMenu(e) {
+    links.forEach(l => l.classList.remove("active"));
+    let d = "";
+    let {width: w, height: h, left: l} = svg.getBoundingClientRect();
+    if(!e)
+    {
+        d =
+        `M5 20 Q5 5 20 5
+        L${w - 20} 5
+        Q${w -5} 5 ${w - 5} 20
+        L${w - 5} ${h - 20}
+        Q${w - 5} ${h - 5} ${w - 20} ${h - 5}
+        L20 ${h - 5}
+        Q5 ${h - 5}
+        5 ${h - 20}
+        L5 20`;
+    }
+    else {
+        const circles = document.querySelectorAll("circle");
+        circles.forEach(c => c.remove());
+        let {left: al, right: ar, width: aw, top: at} = e.currentTarget.getBoundingClientRect();
+        let leftoffset = al - l;
+        let thirdwidth = aw / 3;
+        let newtop = at / 3;
+        d =
+        `M5 20 Q5 5 20 5
+        L${leftoffset - thirdwidth} 5
+
+        C${leftoffset - 20} 5 ${leftoffset - 20} ${newtop} ${leftoffset} ${newtop}
+        L${leftoffset + aw} ${newtop}
+        C${leftoffset + aw + 20} ${newtop} ${leftoffset + aw + 20} 5 ${leftoffset + aw + thirdwidth} 5
+
+        L${w - 20} 5
+
+
+        Q${w - 5} 5 ${w - 5} 20
+        L${w - 5} ${h - 20}
+        Q${w - 5} ${h - 5} ${w - 20} ${h - 5}
+        L20 ${h - 5}
+        Q5 ${h - 5}
+        5 ${h - 20}
+        L5 20`;
+        e.currentTarget.classList.add("active");
+    }
+    path.setAttribute("d", d);
+}
+
+
+links.forEach(a => {
+    a.addEventListener("click", renderMenu);
+});
